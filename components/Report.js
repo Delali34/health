48d9c297e7f29dff84e21a9676dd898c3b9301d4
Delali34 <ernestanyomitse752@gmail.com>
2024-04-11 15:38:05 +0000

    done

--- components/Report.js
+++ components/Report.js
@@ -5,16 +5,17 @@ const PartnerWithUs = () => {
   return (
     <section className="bg-gray-100 py-12 px-4 md:px-0">
       <div className="max-w-4xl mx-auto text-center">
-        <h2 className="text-2xl md:text-4xl font-bold mb-6">
-          Do you want a social issue addressed? Be anonymous.
+        <h2 className="text-xl md:text-3xl text-left font-bold mb-6">
+          Do you want a social issue addressed? We guarantee your
+          confidentiality.
         </h2>
-        <p className="lg:text-lg text-sm text-gray-700 mb-8">
-          Report Fraud, Social Vices, Abuse, Misconduct or anything of
+        <p className="lg:text-lg text-left text-sm text-gray-700 mb-8">
+          Report fraud, social vices, abuse, misconduct or anything of
           importance to society.
         </p>
         <Link href="/contact">
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-8 rounded-full shadow-lg transition duration-300">
-            Complain here...
+            Make Complain here...
           </button>
         </Link>
       </div>
