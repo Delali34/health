6bc9657e72bc060da5c5ed74fcafe6c9cac15052
Delali34 <ernestanyomitse752@gmail.com>
2024-04-11 14:50:22 +0000

    done

--- components/Projects.js
+++ components/Projects.js
@@ -10,15 +10,15 @@ const NGOProjects = () => {
     {
       title: "Star Readers Initiative",
       description:
-        "We developed one of the most exciting reading programmes for basic school children in 5 regions of Ghana. Our young stars are now doing well in digesting complex sentences, making coherent arguments and developing their own storylines…",
+        "We developed one of the most exciting reading programmes for basic school children in 5 regions of Ghana. Our young stars are now doing well in digesting complex sentences, making coherent arguments and developing their own storyline. Some of our beneficiaries are have won national essay writing competitions.",
       testimony:
         "At first, reading was my biggest problem in school. My friends used to mock at me and I felt like dropping out. So when I heard that we are going to have a reading programme, I was the first person to show up. Now I can read like any other student… Thanks to the people who brought this programme to our school.",
       imageUrl: "/education.jpg", // Example image path
     },
     {
-      title: "COVID-19 Frontline Support",
+      title: "Disease Outbreak Response",
       description:
-        "We played frontline roles in the fight against COVID-19 outbreak. This was one of our groundbreaking experiences. We thought we did little but beneficiaries were overwhelmed… We worked with our communities, shared accurate information with them and they took the necessary precautions to protect themselves.",
+        "We played frontline roles in the fight against COVID-19, meningitis, rabbies and other diesease outbreaks.We conducted risk communication and community engagement, dissemenated livesaving infomation and facilitated case referrals to health facilities. We partnered with donors to strengthen health systems and community stuctures among other things. This was one of our groundbreaking experiences. We thought we did little but beneficiaries were overwhelmed… We worked with our communities, shared accurate information with them and they took the necessary precautions to protect themselves.",
       testimony:
         "We thought we did little but beneficiaries were overwhelmed… We worked with our communities, shared accurate information with them and they took the necessary precautions to protect themselves.",
       imageUrl: "/health3.png", // Example image path
@@ -26,15 +26,15 @@ const NGOProjects = () => {
     {
       title: "Community Nutrition Project",
       description:
-        "Our food and nutrition projects were community based. We taught mothers how to cook nutritious foods. And we learnt what different community cultures mean by “food”. ",
+        "Our food and nutrition projects were community based. We taught parents how to cook nutritious foods. We also learnt what “food” means to different cultures.",
       testimony:
         "This nutrition programme taught me a life lesson. I thought I needed lots of money to prepare good food for my family. This programme taught me how to combine our local foods to make my children grow healthy and strong.",
       imageUrl: "/small.jpg", // Example image path
     },
     {
-      title: "Business Capacity Livelihoods",
+      title: "Livelihood Capacity Building",
       description:
-        "Small scale business enterprises are at our heart. This project focused on building capacity of petty traders on managing their businesses in the current era of advanced technology. We helped business owners to learn new ways of managing their finances ….",
+        "Small scale business enterprises are at our heart. This project focused on building capacity of traders, livestock and crop farmers on increasing yield and financial literacy. We expect to expand this project as a nationawide intervention.",
       testimony:
         "How I wish this training programme came earlier to our community. Those times we used to get a lot of money from our businesses but we didn’t manage it well. But with this knowledge I am sure I can save some money and expand my business.",
       imageUrl: "/health3.png", // Example image path
@@ -42,7 +42,7 @@ const NGOProjects = () => {
     {
       title: "Family Planning Intervention",
       description:
-        "We helped families to determine when to have their children with the most convenient intervals. Our family planning interventions targeted the youth and new couples. Our beneficiaries gave heartwarming testimonies….. ",
+        "We helped families to determine when to have their children with the most convenient intervals. Our family planning interventions targeted the youth and new couples. Through this project, family planning accepter rate has increased mainly due to how we addressed the myths in various communities. Our beneficiaries gave heartwarming testimonies.",
       testimony:
         "I never did family planning because the way people talk about family planning was frightening. I now understand that family planning is not only by injection. I didn’t know you have to make your own choice. Now I can have my next child when I am ready.",
       imageUrl: "/health1.jpg", // Example image path
@@ -50,7 +50,7 @@ const NGOProjects = () => {
     {
       title: "Water is Life Project",
       description:
-        "Our water and sanitation projects targeted the rural areas in Upper East, Oti, Volta and Bono regions. The “Water is Life” project was a lifesaving intervention that cannot be forgotten in our records……",
+        "Our water and sanitation projects targeted communities which practiced open defecation and competed for drinking water with animals from shallow open sources. In all, we provided 49 mechanized boreholes and 88 improved tiolet facilities in Upper East, Oti, Volta and Bono regions. This helped reduce water-borne diseases and other communicable diseases.",
       testimony:
         "The “Water is Life” project was a lifesaving intervention that cannot be forgotten in our records.",
       imageUrl: "/city.jpg", // Example image path
@@ -58,7 +58,7 @@ const NGOProjects = () => {
     {
       title: "Health Insurance Enrollment",
       description:
-        "We led a nationwide free registration of children in basic schools and underprivileged communities onto the National Health Insurance Scheme (NHIS). We are grateful to GIZ and partners for funding this activity that reached over 470,000 beneficiaries.  ",
+        "We led a nationwide free registration of children in basic schools and underprivileged communities onto the National Health Insurance Scheme (NHIS). We are grateful to GIZ and our partners for funding this activity that reached over 470,000 beneficiaries.",
       testimony:
         "I was not going to the hospital because my insurance was dead some long time ago. But after the NGO came and renewed it, I started visiting the hospital. That was when I realized I had a serious health problem but thank God I have been treated.",
       imageUrl: "/image5.jpg", // Example image path
@@ -66,7 +66,7 @@ const NGOProjects = () => {
     {
       title: "Mass Health Screening",
       description:
-        "This initiative is a projection of the Universal Health Coverage Agenda…. Bringing healthcare to the doorstep of communities. Through this project, we provided free health screening to over 1.6 million Ghanaians. We did not leave out those affected by the Akosombo Dam spillage disaster in the Volta region. ",
+        "We are experts in Health screening for early detection and treatment of diseases. Our latest health screening was in partnership with GIZ where we reached over 1.6 million persons living in Ghana in 2023. We also provided free health screening to those affected by the Akosombo Dam spillage disaster in Ghana.",
       testimony:
         "Through this project, we provided free health screening to over 1.6 million Ghanaians. We did not leave out those affected by the Akosombo Dam spillage disaster in the Volta region.",
       imageUrl: "/image2.jpg", // Example image path
@@ -74,7 +74,7 @@ const NGOProjects = () => {
     {
       title: "Inform for Health (i-4-H) Project",
       description:
-        "Africa Health Promotion Organization rolled out the inform for health (i-4-H) project. We conducted interactive radio programme featuring experienced public health professionals and general physicians with phone in sessions. The feedback on the importance of this radio driven health education programme was unimaginably heartwarming. ",
+        "Africa Health Promotion Organization rolled out the inform for health (i-4-H) project. We conducted interactive radio and TV programs featuring experienced public health professionals and physicians with phone-in sessions. We addressed long standing myths and reached out to 10million Ghanaians through theses media platforms",
       testimony:
         "The feedback on the importance of this radio-driven health education programme was unimaginably heartwarming.",
       imageUrl: "/image4.jpg", // Example image path
@@ -82,7 +82,7 @@ const NGOProjects = () => {
     {
       title: "Non-Communicable Diseases Awareness",
       description:
-        "Non-communicable diseases are cutting short the lives of our adult population. This is not just an issue in Ghana… it is a global phenomenon. Raising awareness about the increasing rate of NCDs in Ghana is as important as building hospitals for treatment. We engaged our communities with priority to those aged 40 years and above. The outcome was encouraging….",
+        "Non-communicable diseases are cutting short the lives of our adult population. This is not just an issue in Ghana but a global public health menace. Our effort in raising public awareness about NCDs prevention was as vigorous as the effort needed for treatment. We engaged our communities with priority to those aged 40 years and above.",
       testimony:
         "I looked very healthy so I never thought I could be having NCD. When the NGO came here and encouraged us to go for regular checkup, I was reluctant but my wife insisted. After the checkup, they said my blood pressure was above normal. I thank God, they showed me what to do to remain healthy.",
       imageUrl: "/vaccine.jpg", // Example image path
@@ -90,14 +90,14 @@ const NGOProjects = () => {
     {
       title: "Climate Change Initiative",
       description:
-        "The Africa HPO is committed to promoting the Paris agreement on climate change. Our engagement with communities focusses on responsible farming practices and preservation of water bodies. As of November 2023, our initiative led to the growing of 1,502 trees. Our target is to grow 5,000 trees by the end of 2024.",
+        "Africa HPO is committed to promoting the Paris agreement on climate change. Our engagement with communities focusses on responsible farming practices, preservation of water bodies and investment in greening. As of November 2023, our initiative led to the growing of 1,502 trees. Our target is to grow 5,000 trees by the end of 2024.",
       testimony: "",
       imageUrl: "/image1.jpg", // Example image path
     },
     {
       title: "Urban Health Promotion Campaign",
       description:
-        "Our urban health initiatives focus on air pollution in cities. Our campaign targeted areas with activities that lead to the emission of environmental pollutants. We are working to mitigate the preventable deaths of 8.3 million people annually from air pollution.",
+        "The urban health initiatives focus on air pollution in cities. Our campaign targeted areas with activities that result in the emission of indoor and outdoor pollutants. We are working to mitigate the preventable deaths of 8.3 million people annually from air pollution.",
       testimony: "",
       imageUrl: "/image5.jpg", // Example image path
     },
--- components/Services.js
+++ components/Services.js
@@ -1,12 +1,128 @@
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import Image from "next/image";
+// import { MdOutlineLocalHospital } from "react-icons/md";
+// import { FaSyringe } from "react-icons/fa";
+// import { GiFruitBowl } from "react-icons/gi";
+// import { FaBaby } from "react-icons/fa";
+// import { IoMdHappy } from "react-icons/io";
+// import { AiFillTool } from "react-icons/ai";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+
+// const serviceContent = {
+//   heading: {
+//     headingSubTitle: "Our Awesome Services",
+//     headingTitle: "Projects",
+//     description: " ",
+//   },
+//   items: [
+//     {
+//       icon: "/prevent.jpg",
+//       title: "Community Nutrition Project",
+//       description:
+//         "Our food and nutrition projects were community based. We taught parents how to cook nutritious foods. We also learnt what “food” means to different cultures.",
+//     },
+//     {
+//       icon: "/vaccine.jpg",
+//       title: "Disease Outbreak Response",
+//       description:
+//         "We played frontline roles in the fight against COVID-19, meningitis, rabbies and other diesease outbreaks.We conducted risk communication and community engagement, dissemenated livesaving infomation and facilitated case referrals to health facilities. We partnered with donors to strengthen health systems and community stuctures among other things. This was one of our groundbreaking experiences. We thought we did little but beneficiaries were overwhelmed… We worked with our communities, shared accurate information with them and they took the necessary precautions to protect themselves.",
+//     },
+//     {
+//       icon: "/education.jpg",
+//       title: "Star Readers Initiative",
+//       description:
+//         " We developed one of the most exciting reading programmes for basic school children in 5 regions of Ghana. Our young stars are now doing well in digesting complex sentences, making coherent arguments and developing their own storyline. Some of our beneficiaries are have won national essay writing competitions.",
+//     },
+//     {
+//       icon: "/maternal.jpg",
+//       title: "Livelihood Capacity Building",
+//       description:
+//         " Small scale business enterprises are at our heart. This project focused on building capacity of traders, livestock and crop farmers on increasing yield and financial literacy. We expect to expand this project as a nationawide intervention.",
+//     },
+//     {
+//       icon: "/mental.jpg",
+//       title: "Family Planning Intervention",
+//       description:
+//         "We helped families to determine when to have their children with the most convenient intervals. Our family planning interventions targeted the youth and new couples. Through this project, family planning accepter rate has increased mainly due to how we addressed the myths in various communities. Our beneficiaries gave heartwarming testimonies.",
+//     },
+//     {
+//       icon: "/develop.jpg",
+//       title: "Water is Life Project",
+//       description:
+//         " Our water and sanitation projects targeted communities which practiced open defecation and competed for drinking water with animals from shallow open sources. In all, we provided 49 mechanized boreholes and 88 improved tiolet facilities in Upper East, Oti, Volta and Bono regions. This helped reduce water-borne diseases and other communicable diseases.",
+//     },
+//   ],
+// };
+// const paragraphStyle = {
+//   WebkitLineClamp: 2,
+//   WebkitBoxOrient: "vertical",
+//   overflow: "hidden",
+//   display: "-webkit-box",
+// };
+
+// const Services = () => {
+//   useEffect(() => {
+//     AOS.init({
+//       duration: 700,
+//       easing: "slide",
+//       once: true,
+//     });
+//   });
+//   const [isOpen, setIsOpen] = useState(false);
+//   return (
+//     <section className="py-20 bg2 font-mont">
+//       <div className="container px-4 mx-auto">
+//         <h1 className="text-3xl text-[#092862] text-center font-bold">
+//           {serviceContent.heading.headingTitle}
+//         </h1>
+//         <div className=" max-w-[450px] mx-auto pt-2">
+//           <p className=" text-[13px]">{serviceContent.heading.description}</p>
+//         </div>
+
+//         <div className="grid  gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+//           {serviceContent.items.map((items, index) => {
+//             index *= 100;
+//             return (
+//               <div
+//                 className="bg-light mt-10 p-5 flex flex-col justify-center items-center  shadow-lg"
+//                 data-aos="fade-up"
+//                 data-aos-delay={index}
+//               >
+//                 <div>
+//                   <Image src={items.icon} width={500} height={500} alt="" />
+//                 </div>
+//                 <div className="pt-3">
+//                   <h1 className="text-xl text-[#092862] text-center font-medium">
+//                     {items.title}
+//                   </h1>
+//                   <p
+//                     style={isOpen ? null : paragraphStyle}
+//                     className="text-sm text-[#092862] pt-2 text-left"
+//                   >
+//                     {items.description}
+//                   </p>
+//                   <p
+//                     className="text-blue-400 underline cursor-pointer"
+//                     onClick={() => setIsOpen(!isOpen)}
+//                   >
+//                     {isOpen ? "read less" : "read more"}
+//                   </p>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Services;
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { MdOutlineLocalHospital } from "react-icons/md";
-import { FaSyringe } from "react-icons/fa";
-import { GiFruitBowl } from "react-icons/gi";
-import { FaBaby } from "react-icons/fa";
-import { IoMdHappy } from "react-icons/io";
-import { AiFillTool } from "react-icons/ai";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -21,39 +137,46 @@ const serviceContent = {
       icon: "/prevent.jpg",
       title: "Community Nutrition Project",
       description:
-        "Our food and nutrition projects were community based. We taught mothers how to cook nutritious foods. And we learnt what different community cultures mean by “food”.",
+        "Our food and nutrition projects were community based. We taught parents how to cook nutritious foods. We also learnt what “food” means to different cultures.",
     },
     {
       icon: "/vaccine.jpg",
-      title: "COVID-19 Frontline Support",
+      title: "Disease Outbreak Response",
       description:
-        "We played frontline roles in the fight against COVID-19 outbreak. This was one of our groundbreaking experiences. We thought we did little but beneficiaries were overwhelmed… We worked with our communities, shared accurate information with them and they took the necessary precautions to protect themselves.",
+        "We played frontline roles in the fight against COVID-19, meningitis, rabbies and other diesease outbreaks.We conducted risk communication and community engagement, dissemenated livesaving infomation and facilitated case referrals to health facilities. We partnered with donors to strengthen health systems and community stuctures among other things. This was one of our groundbreaking experiences. We thought we did little but beneficiaries were overwhelmed… We worked with our communities, shared accurate information with them and they took the necessary precautions to protect themselves.",
     },
     {
       icon: "/education.jpg",
       title: "Star Readers Initiative",
       description:
-        " We developed one of the most exciting reading programmes for basic school children in 5 regions of Ghana. Our young stars are now doing well in digesting complex sentences, making coherent arguments and developing their own storylines…",
+        " We developed one of the most exciting reading programmes for basic school children in 5 regions of Ghana. Our young stars are now doing well in digesting complex sentences, making coherent arguments and developing their own storyline. Some of our beneficiaries are have won national essay writing competitions.",
     },
     {
       icon: "/maternal.jpg",
       title: "Livelihood Capacity Building",
       description:
-        " Small scale business enterprises are at our heart. This project focused on building capacity of petty traders on managing their businesses in the current era of advanced technology. We helped business owners to learn new ways of managing their finances ….",
+        " Small scale business enterprises are at our heart. This project focused on building capacity of traders, livestock and crop farmers on increasing yield and financial literacy. We expect to expand this project as a nationawide intervention.",
     },
     {
       icon: "/mental.jpg",
       title: "Family Planning Intervention",
       description:
-        "We helped families to determine when to have their children with the most convenient intervals. Our family planning interventions targeted the youth and new couples. Our beneficiaries gave heartwarming testimonies…..",
+        "We helped families to determine when to have their children with the most convenient intervals. Our family planning interventions targeted the youth and new couples. Through this project, family planning accepter rate has increased mainly due to how we addressed the myths in various communities. Our beneficiaries gave heartwarming testimonies.",
     },
     {
       icon: "/develop.jpg",
       title: "Water is Life Project",
       description:
-        " Our water and sanitation projects targeted the rural areas in Upper East, Oti, Volta and Bono regions. The “Water is Life” project was a lifesaving intervention that cannot be forgotten in our records……",
+        " Our water and sanitation projects targeted communities which practiced open defecation and competed for drinking water with animals from shallow open sources. In all, we provided 49 mechanized boreholes and 88 improved tiolet facilities in Upper East, Oti, Volta and Bono regions. This helped reduce water-borne diseases and other communicable diseases.",
     },
   ],
+};
+
+const paragraphStyle = {
+  WebkitLineClamp: 2,
+  WebkitBoxOrient: "vertical",
+  overflow: "hidden",
+  display: "-webkit-box",
 };
 
 const Services = () => {
@@ -64,6 +187,15 @@ const Services = () => {
       once: true,
     });
   });
+
+  const [isOpen, setIsOpen] = useState(serviceContent.items.map(() => false));
+
+  const toggleDescription = (index) => {
+    const newIsOpen = [...isOpen];
+    newIsOpen[index] = !newIsOpen[index];
+    setIsOpen(newIsOpen);
+  };
+
   return (
     <section className="py-20 bg2 font-mont">
       <div className="container px-4 mx-auto">
@@ -75,23 +207,32 @@ const Services = () => {
         </div>
 
         <div className="grid  gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {serviceContent.items.map((items, index) => {
-            index *= 100;
+          {serviceContent.items.map((item, index) => {
             return (
               <div
-                className="bg-light mt-10 p-5 flex flex-col justify-center items-center  shadow-lg"
+                key={index}
+                className="bg-light mt-10 p-5 flex flex-col  items-center  shadow-lg"
                 data-aos="fade-up"
-                data-aos-delay={index}
+                data-aos-delay={index * 100}
               >
                 <div>
-                  <Image src={items.icon} width={500} height={500} alt="" />
+                  <Image src={item.icon} width={500} height={500} alt="" />
                 </div>
                 <div className="pt-3">
                   <h1 className="text-xl text-[#092862] text-center font-medium">
-                    {items.title}
+                    {item.title}
                   </h1>
-                  <p className="text-sm text-[#092862] pt-2 text-left">
-                    {items.description}
+                  <p
+                    style={isOpen[index] ? null : paragraphStyle}
+                    className="text-sm text-[#092862] pt-2 text-left"
+                  >
+                    {item.description}
+                  </p>
+                  <p
+                    className="text-blue-400 underline cursor-pointer"
+                    onClick={() => toggleDescription(index)}
+                  >
+                    {isOpen[index] ? "read less" : "read more"}
                   </p>
                 </div>
               </div>
